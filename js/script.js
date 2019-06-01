41336c14b18a647bdc01a87b4af2aeacd9f4e632
Lipin Dmitriy <lipindim@gmail.com>
2019-06-01 14:32:14 +1000

    Update

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function convertTemperature() {
-    var temperatureStr = document.getElementById('#temperatureC').value;
-    var temperature = parseInt(temperatureStr);
-    if (isNaN(temperature)) {
-        alert("Dear friend! U inputed not a number, please try again!");
-    } else {
-        document.getElementById('#temperatureF').value = (9 / 5) * temperature + 32;
-    }
-}
